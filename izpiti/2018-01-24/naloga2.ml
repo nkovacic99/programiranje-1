9498039798218fac2681d5cd2f0d36d4ec05d0d7
nkovacic99 <nace.kovacic007@gmail.com>
2020-01-14 17:15:56 +0100

    process

--- izpiti/2018-01-24/naloga2.ml
+++
@@ -1,6 +0,0 @@
-type 'a filter_tree =
-    | Vozlisce of 'a filter_tree * int *  'a filter_tree 
-    | List of 'a list
-
-let test_tree  = Vozlisce(Vozlisce(List[1], 5, List[]), 10, Vozlisce(List[0], 15, List[19; 20]))
-
