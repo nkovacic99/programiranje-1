4baf2aefd2170fa16e3065aae32ae8962011c332
nkovacic99 <nace.kovacic007@gmail.com>
2020-01-27 23:37:04 +0100

    was solving some more exercises

--- izpiti/2018-01-24/naloga1.ml
+++ izpiti/2018-01-24/naloga1.ml
@@ -5,9 +5,11 @@ let rec map2_opt f ls1 ls2 ls =
         | (ls1, []) -> None
         | (x :: xs, y :: ys) -> map2_opt f xs ys (f x y :: ls) 
 
-(* let map2_opt' f ls1 ls2 = 
-    let rec map' f acc = function
-        | ([], []) -> Some([acc])
-        | (_, []) | ([], _) -> None
-        |  (x :: xs, y :: ys) -> map' f ((f x y) :: acc)
-    in map' f [] ls1 ls2 *)
+let map2_opt f ls1 ls2 =
+    if List.length ls1 != List.length ls2 then None
+    else
+        let rec map2_aux acc ls1 ls2= 
+            match (ls1, ls2) with
+                | ([], _) | (_, []) -> Some (List.rev acc)
+                | (x :: xs, y :: ys) -> map2_aux ((f x y) :: acc) xs ys
+            in map2_aux [] ls1 ls2
